d7e271814ce062bbcb6acea2ba57c300eeb8ee94
pingmommy <mcar8764@naver.com>
2024-05-18 17:24:21 +0900

    분기배포

--- pages/boards/[boardId]/index.js
+++ pages/boards/[boardId]/index.js
@@ -11,9 +11,9 @@ export default function BoardsPage() {
   );
 }
 
-export const getServerSideProps = () => {
-  // 만약 서버사이트 렌더링을 하는 페이지라면?=> out폴더로 생성 불가
-  //                                       => next.config.js에서 exportPathMap으로 현재페이지 제외시키기
-  // out 폴더는 완성된 html 파일을 완성된 걸 다운로드만 받아서 오는 건데, 지금 여기는 서버에서 백엔드에 요청에서 data가 올 때까지 기다린 다음, 화면을 만들어주는 곳이기 때문에 당연히 out 폴더 생성이 안되는 것이다.
-  // 서버사이드렌더링이 들어가 있는 페이지들은 export path할 때 out폴더가 안 만들어지도록 next.config.js 파일에 설정을 해주어야 한다.
-};
+//export const getServerSideProps = () => {
+// 만약 서버사이트 렌더링을 하는 페이지라면?=> out폴더로 생성 불가
+//                                       => next.config.js에서 exportPathMap으로 현재페이지 제외시키기
+// out 폴더는 완성된 html 파일을 완성된 걸 다운로드만 받아서 오는 건데, 지금 여기는 서버에서 백엔드에 요청에서 data가 올 때까지 기다린 다음, 화면을 만들어주는 곳이기 때문에 당연히 out 폴더 생성이 안되는 것이다.
+// 서버사이드렌더링이 들어가 있는 페이지들은 export path할 때 out폴더가 안 만들어지도록 next.config.js 파일에 설정을 해주어야 한다.
+//};
