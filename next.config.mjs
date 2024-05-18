daa5b3ac3aad56c59bb9f8b5774d063af7d0537f
pingmommy <mcar8764@naver.com>
2024-05-18 17:22:25 +0900

    분기배포

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  generateBuildId: () => "codecamp-deploy-project",
+
+  // 아래 주소들만 out 폴더에 만들어줘!(getServerSideProps 있는 페이지는 제외)
+  exportPathMap: () => ({
+    "/": { page: "/" },
+    "/boards": { page: "/boards" },
+    "/404": { page: "404" },
+  }),
 };
 
 export default nextConfig;
